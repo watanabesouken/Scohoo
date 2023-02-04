648874cab5c63a2de2350b12a110eee6115fd221
watanabesouken <watasou.0909@gmail.com>
2023-02-04 21:16:12 +0900

    初回

---
+++ assets/swift_bk_custom.js
@@ -0,0 +1,6 @@
+//$(document).on('change', '[name^="snize_facet"]', function(){
+//  setTimeout(function () {
+//    const val = location.search;
+//    window.location.href = "https://shohoo.net/pages/searchresults" + val ;
+//  }, 500);
+//});
